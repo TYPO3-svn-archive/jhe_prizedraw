02734d5b99bbf7d6ad939592957a984748e68a2c
jhernst <jhernst@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-02 14:22:44 +0000

    Erweiterung des Formulars, erste Validierungen
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jhe_prizedraw/trunk@33852 735d13b6-9817-0410-8766-e36946ffe9aa

--- mod1/conf.php
+++ mod1/conf.php
@@ -3,7 +3,7 @@
 	// DO NOT REMOVE OR CHANGE THESE 2 LINES:
 $MCONF['name'] = 'web_txjheprizedrawM1';
 $MCONF['script'] = '_DISPATCH';
-	
+
 $MCONF['access'] = 'user,group';
 
 $MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
--- mod1/index.php
+++ mod1/index.php
@@ -24,13 +24,31 @@
 /**
  * [CLASS/FUNCTION INDEX of SCRIPT]
  *
- * Hint: use extdeveval to insert/update function index above.
+ *
+ *
+ *   57: class  tx_jheprizedraw_module1 extends t3lib_SCbase
+ *   65:     function init()
+ *   82:     function menuConfig()
+ *   99:     function main()
+ *  118:     function jumpToUrl(URL)
+ *  170:     function printContent()
+ *  181:     function moduleContent()
+ *  288:     function getNoOfRecordsPerPage($table, $pid)
+ *
+ * TOTAL FUNCTIONS: 7
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
  */
 
 
 $LANG->includeLLFile('EXT:jhe_prizedraw/mod1/locallang.xml');
+
 require_once(PATH_t3lib . 'class.t3lib_scbase.php');
 require_once(PATH_t3lib . 'class.t3lib_befunc.php');
+require_once(PATH_t3lib . 'class.t3lib_div.php');
+require_once(PATH_t3lib . 'class.t3lib_flashmessage.php');
+
+
 $BE_USER->modAccess($MCONF,1);	// This checks permissions and exits if the users has no permission for entry.
 	// DEFAULT initialization of a module [END]
 
@@ -45,9 +63,10 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
 				var $pageinfo;
 
 				/**
-				 * Initializes the Module
-				 * @return	void
-				 */
+ * Initializes the Module
+ *
+ * @return	void
+ */
 				function init()	{
 					global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
 
@@ -61,10 +80,10 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
 				}
 
 				/**
-				 * Adds items to the ->MOD_MENU array. Used for the function menu selector.
-				 *
-				 * @return	void
-				 */
+ * Adds items to the ->MOD_MENU array. Used for the function menu selector.
+ *
+ * @return	void
+ */
 				function menuConfig()	{
 					global $LANG;
 					$this->MOD_MENU = Array (
@@ -77,11 +96,11 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
 				}
 
 				/**
-				 * Main function of the module. Write the content to $this->content
-				 * If you chose "web" as main module, you will need to consider the $this->id parameter which will contain the uid-number of the page clicked in the page tree
-				 *
-				 * @return	[type]		...
-				 */
+ * Main function of the module. Write the content to $this->content
+ * If you chose "web" as main module, you will need to consider the $this->id parameter which will contain the uid-number of the page clicked in the page tree
+ *
+ * @return	[type]		...
+ */
 				function main()	{
 					global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
 
@@ -89,14 +108,13 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
 					// The page will show only if there is a valid page and if this page may be viewed by the user
 					$this->pageinfo = t3lib_BEfunc::readPageAccess($this->id,$this->perms_clause);
 					$access = is_array($this->pageinfo) ? 1 : 0;
-				
+
 					if (($this->id && $access) || ($BE_USER->user['admin'] && !$this->id))	{
 
 							// Draw the header.
 						$this->doc = t3lib_div::makeInstance('mediumDoc');
 						$this->doc->backPath = $BACK_PATH;
-						$this->doc->form='<form id="tx_jheprizedraw_form" action="" method="post" enctype="multipart/form-data">';
-
+						$this->doc->form='<form action="" id="tx_jheprizedraw_form" name="editform" method="post" enctype="multipart/form-data">';
 							// JavaScript
 						$this->doc->JScode = '
 							<script language="javascript" type="text/javascript">
@@ -113,8 +131,6 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
 							</script>
 						';
 
-						//$headerSection = $this->doc->getHeader('pages',$this->pageinfo,$this->pageinfo['_thePath']).'<br />'.$LANG->sL('LLL:EXT:lang/locallang_core.xml:labels.path').': '.t3lib_div::fixed_lgd_pre($this->pageinfo['_thePath'],50);
-
 						$this->doc->inDocStylesArray[] = t3lib_div::getURL(t3lib_extMgm::extPath('jhe_prizedraw').'res/css/be.css');
 						
 						$this->content.=$this->doc->startPage($LANG->getLL('title'));
@@ -123,17 +139,16 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
 						$this->content.=$this->doc->section('',$this->doc->funcMenu($headerSection,t3lib_BEfunc::getFuncMenu($this->id,'SET[function]',$this->MOD_SETTINGS['function'],$this->MOD_MENU['function'])));
 						$this->content.=$this->doc->divider(5);
 
-
 						// Render content:
 						$this->moduleContent();
-
-
+						
 						// ShortCut
-						/*if ($BE_USER->mayMakeShortcut())	{
+						if ($BE_USER->mayMakeShortcut())	{
 							$this->content.=$this->doc->spacer(20).$this->doc->section('',$this->doc->makeShortcutIcon('id',implode(',',array_keys($this->MOD_MENU)),$this->MCONF['name']));
-						}*/
-
+						}
+					
 						$this->content.=$this->doc->spacer(10);
+						
 					} else {
 							// If no access or if ID == zero
 
@@ -145,14 +160,14 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
 						$this->content.=$this->doc->spacer(5);
 						$this->content.=$this->doc->spacer(10);
 					}
-				
+
 				}
 
 				/**
-				 * Prints out the module HTML
-				 *
-				 * @return	void
-				 */
+ * Prints out the module HTML
+ *
+ * @return	void
+ */
 				function printContent()	{
 
 					$this->content.=$this->doc->endPage();
@@ -160,65 +175,37 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
 				}
 
 				/**
-				 * Generates the module content
-				 *
-				 * @return	void
-				 */
+ * Generates the module content
+ *
+ * @return	void
+ */
 				function moduleContent(){
 					global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
-					
-					
-				//$listGroupNames = t3lib_BEfunc::TYPO3_copyRightNotice();
-				//var_dump($listGroupNames);	
-					
-					
-				//include_once(t3lib_extMgm::extPath('date2cal') . 'src/class.jscalendar.php');
-				//var_dump(include_once(t3lib_extMgm::extPath('date2cal') . 'src/class.jscalendar.php'));
-					
-				
-				//if (class_exists('JSCalender')){
-				//	$JSCalendar = JSCalendar::getInstance();
-				//} else {
-				//	echo "Class JSCalender not found!";
-				//}
-				
-				//
-				//$this->markerArray['###FIELD###'] .= $JSCalendar->render($value);
-				//if (($jsCode = $JSCalendar->getMainJS()) != '') {
-				//	$GLOBALS['TSFE']->additionalHeaderData['powermail_date2cal'] = $jsCode;
-				//}
-					
-				//var_dump(include_once(t3lib_extMgm::siteRelPath('date2cal') . '/src/class.jscalendar.php'));
-										
+
 					switch((string)$this->MOD_SETTINGS['function']){
                     	case 1:
-                    		                   		                   		
+
                     		$uid = $this->pageinfo['uid'];
-                    		
+
                     		if($uid){
-                    			$resFeUserRecords = $GLOBALS['TYPO3_DB']->exec_SELECTquery('COUNT(*)', 'fe_users', 'pid = ' . $uid . '' );
-	                       		$resFeUserRecordsArr = $GLOBALS['TYPO3_DB']->sql_fetch_row($resFeUserRecords) or die (mysql_error());
-        	            		$noFeUserRecords = $resFeUserRecordsArr[0];
-            	        		
-                	    		$resAddressRecords = $GLOBALS['TYPO3_DB']->exec_SELECTquery('COUNT(*)', 'tt_address', 'pid = '. $uid .'');
-                    			$resAddressRecordsArr = $GLOBALS['TYPO3_DB']->sql_fetch_row($resAddressRecords) or die (mysql_error());
-                    			$noAddressRecords = $resAddressRecordsArr[0];
-                    			
+                    			$noFeUserRecords = $this->getNoOfRecordsPerPage('fe_users', $uid);
+                    			$noAddressRecords = $this->getNoOfRecordsPerPage('tt_address', $uid);
+
 	                    		$sumRelRecords = $noFeUserRecords + $noAddressRecords;
         	           		}
-                    		 
+
 
                     		if(!$uid) {
-								$content = $LANG->getLL('error_root'); 
+								$content = $LANG->getLL('error_root');
 							} else if($sumRelRecords == 0){
 									$content = $LANG->getLL('error_no_records');
 								} else {
-                        			$content .= '<form id="jet" name="jet" method="post" action="">
+                        			$content .= '
   													<p>
     													<label for="no_of_records">' . $LANG->getLL('lbl_no_of_records') . '</label>
     													<input name="no_of_records" type="text" id="no_of_records" size="4" />
   													</p>';
-									
+
                         			if(!$noFeUserRecords) {
                         				$content .=	'	<input name="adressOnly" type="hidden" id="adressOnly" value="true" />';
                         			} else if(!$noAddressRecords) {
@@ -227,41 +214,94 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
                         				$content .=	'	<p>
     														<label for="record_type">' . $LANG->getLL('lbl_record_type') . '</label>
     														<select name="record_type" id="record_type">
-      															<option value=""selected="selected">' . $LANG->getLL('lbl_select') . '</option>
+      															<option value="" selected="selected">' . $LANG->getLL('lbl_select') . '</option>
     															<option value="fe_user">' . $LANG->getLL('lbl_fe_user') . '</option>
       															<option value="tt_address">' . $LANG->getLL('lbl_tt_address') . '</option>
       															<option value="both">' . $LANG->getLL('lbl_both') . '</option>
 	    													</select>
   														</p>';
                         			}
-									
+
 									$content .= '	<p>
   														<label class="header">' . $LANG->getLL('lbl_period') . '</label>
   													</p>
   													<p>
     													<label for="period_begin">' . $LANG->getLL('lbl_period_begin') . '</label>
-    													<input type="text" name="period_begin" id="period_begin" />
+    													<input type="text" name="period_begin" id="tceforms-datetimefield-period_begin" />
   													</p>
   													<p>
     													<label for="period_end">' . $LANG->getLL('lbl_period_end') . '</label>
-    													<input type="text" name="period_end" id="period_end" />
+    													<input type="text" name="period_end" id="tceforms-datetimefield-period_end" />
 	  												</p>
   													<p>
     													<input type="submit" name="bt_execute" id="bt_execute" value="' . $LANG->getLL('lbl_bt_execute') . '" />
   													</p>
-												</form>';
-							
-									$content.='<br />This is the GET/POST vars sent to the script:<br />'.
+												';
+								}
+
+							if($GLOBALS['_POST']['bt_execute']){
+								$errorMessage = '';
+								
+								//$checkdate = checkdate('2','30','2010');
+								//$errorMessage .= $checkdate . '<br />';
+																
+								//Validierung der Eingabe
+								if(!$GLOBALS['_POST']['no_of_records']) {
+									$errorMessage .= '<li>Bitte geben Sie an, wie viele Datensätze Sie ausgeben lassen wollen.</li>';
+								} else if(!is_numeric(!$GLOBALS['_POST']['no_of_records'])) {
+									$errorMessage .= '<li>Bitte geben Sie eine numerische Anzahl der gewünschten Datensätze ein.</li>';
+								}
+								if(!$GLOBALS['_POST']['record_type']){
+									$errorMessage .= '<li>Bitte wählen Sie den Typ der auszuwählenden Datensätze aus.</li>';
+								}
+
+								/*if($GLOBALS['_POST']['period_begin'] && !$GLOBALS['_POST']['period_end']){
+									$errorMessage .= '<li>Huiuiui!</li>';
+								}*/
+
+								if($errorMessage) {
+									$message = t3lib_div::makeInstance(
+                	        			't3lib_FlashMessage', 
+                	        			'<ul>' . $errorMessage . '</ul>', 
+                	        			'Fehler bei der Eingabe',
+                	        			t3lib_FlashMessage::ERROR,
+                	        			FALSE
+                	        		);
+                	        		$content .= $message->render();
+								} else {
+									$content = '';
+									
+									$noOfRecords = $GLOBALS['_POST']['no_of_records'];
+									$recordType = $GLOBALS['_POST']['record_type'];
+									$periodBegin = $GLOBALS['_POST']['period_begin'];
+									$periodEnd = $GLOBALS['_POST']['period_end'];
+									
+									$content .= 'Sie möchten '. $noOfRecords .' Datensätze vom Typ ' . $recordType . ' auswählen lassen.';
+									if($periodBegin && !$periodEnd) {
+										$content .= '<br />Sie erhalten Ergebnisse ab dem ' . $periodBegin . '.';
+									} else if (!$periodBegin && $periodEnd){
+										$content .= '<br />Sie erhalten Ergbenisse bis zum ' . $periodEnd . '.';
+									} else if ($periodBegin && $periodEnd) {
+										$content .= '<br />Sie erhalten Ergbenisse vom ' . $periodBegin . ' bis zum ' . $periodEnd . '.';
+									} else {
+										$content .= '<br />Sie verzichten auf eine zeitliche Eingrenzung.';
+									}
+								}
+
+								
+                	        }
+
+                	        $content.='<br />This is the GET/POST vars sent to the script:<br />'.
 											'GET:'.t3lib_div::view_array($_GET).'<br />'.
 											'POST:'.t3lib_div::view_array($_POST).'<br />'.
 											'';
-								}
-							
+                	        
                 	        $this->content.=$this->doc->section('',$content,0,1);
+
 						break;
                         case 2:
                         	if(!$this->pageinfo['uid']) {
-								$content = $LANG->getLL('error_root'); 
+								$content = $LANG->getLL('error_root');
 							} else {
                         		$content.='';
                             	$content.='<br />This is the GET/POST vars sent to the script:<br />'.
@@ -273,8 +313,24 @@ class  tx_jheprizedraw_module1 extends t3lib_SCbase {
                         break;
 					}
 				}
+
+	/**
+	 * [Describe function...]
+	 *
+	 * @param	[type]		$table: ...
+	 * @param	[type]		$pid: ...
+	 * @return	[type]		...
+	 */
+				function getNoOfRecordsPerPage($table, $pid) {
+
+					$resRecords = $GLOBALS['TYPO3_DB']->exec_SELECTquery('COUNT(*)', $table, 'pid = ' . $pid . '' );
+	                $resRecordsArr = $GLOBALS['TYPO3_DB']->sql_fetch_row($resRecords) or die (mysql_error());
+
+	                return $resRecordsArr[0];
+
+				}
 			}
-					
+
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/jhe_prizedraw/mod1/index.php'])	{
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/jhe_prizedraw/mod1/index.php']);
 }
